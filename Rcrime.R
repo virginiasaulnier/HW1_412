2b04c744bb2fe7c94be66f0f43c49a3c53b04795
virginiasaulnier <vsaulnier@luc.edu>
2016-06-15 10:33:00 -0500

    added plot and comments

--- Rcrime.R
+++ Rcrime.R
@@ -1,6 +1,6 @@
 
 library(dplyr)
-
+#create a function to read in files, also has ability to skip extra header line, uses headers as column titles and returns data
 readin <- function(wd,file,skip){
   setwd(wd)
   dataframe = readLines(file)
@@ -14,32 +14,34 @@ readin <- function(wd,file,skip){
   return(data)
 }
 
-alleylights <- readin('/Users/VirginiaSaulnier/Desktop/412_hw1','311_Service_Requests_-_Alley_Lights_Out.csv',TRUE)
-crime <- readin('/Users/VirginiaSaulnier/Desktop/412_hw1','Crimes2016.csv',FALSE)
+alleylights <- readin('/Users/VirginiaSaulnier/Desktop/412_hw1','311_Service_Requests_-_Alley_Lights_Out.csv',TRUE)#read in alley lights file using funtion readin
+crime <- readin('/Users/VirginiaSaulnier/Desktop/412_hw1','Crimes2016.csv',FALSE)#read in crime info using funtion readin
 
 colnames(alleylights) <- c("Request","Status","Completion", "Number","Type", "Address", "Zip", "1","2","Ward","District","Community", "Latitude","Longitude","Location")
 
-alley16 <- alleylights[grep("2016", alleylights$Request), ]
+alley16 <- alleylights[grep("2016", alleylights$Request), ]#filter out 2016
 alley16$date_diff <- as.Date(as.character(alley16$Completion),format="%m/%d/%y")-
-  as.Date(as.character(alley16$Request), format="%m/%d/%y")
+  as.Date(as.character(alley16$Request), format="%m/%d/%y")#get date diff between date service was requested and date completed create new column to store
 
 
-service_ave = alley16 %>%
+service_ave = alley16 %>%#create new database(service_ave)with repair time average by district
   group_by(District) %>%
   summarise(repairtime = round(mean(date_diff, na.rm=TRUE),digits = 1)) %>%
   arrange(District) 
 group_by(alley16, District)
 
-crime_counts = crime %>%
+crime_counts = crime %>% #create new database(crime_counts) using the number of crimes per district, calculated by counting the instance of crimes(rows) per district
   group_by(District) %>%
   summarise(crimetotal=length(Primary.Type)) %>%
 arrange(District)
 
-results = full_join(crime_counts, service_ave, by = "District")%>%
+results = full_join(crime_counts, service_ave, by = "District")%>% #combine two data sets and arrage by crime total
   arrange(crimetotal)
 
-#colMax <- function(data) sapply(data, max, na.rm = TRUE)
-dat$MAX <- apply(results[,-1],1,max,na.rm=TRUE) 
+plot(results$crimetotal, results$repairtime)
+#below is the start of print out info, will add if I have time
 
-cat("The district with the slowest repair time is the number of crimes this year was")
-cat("The district with the quickest repair time is the number of crimes this year was",dat$MAX)
+#dat$MAX <- apply(results[,-1],1,max,na.rm=TRUE) 
+
+#cat("The district with the slowest repair time is the number of crimes this year was")
+#cat("The district with the quickest repair time is the number of crimes this year was",dat$MAX)
