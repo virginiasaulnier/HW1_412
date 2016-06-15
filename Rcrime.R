a788a450e6ba73d77ebf8c13a82fe09a523843fa
virginiasaulnier <vsaulnier@luc.edu>
2016-06-15 00:36:59 -0500

    updated R code

--- Rcrime.R
+++ Rcrime.R
@@ -1,26 +1,45 @@
 
 library(dplyr)
-setwd('/Users/VirginiaSaulnier/Desktop/Hw1_412')
-all_content = readLines("311_Service_Requests_-_Alley_Lights_Out.csv")
-skip_first = all_content[-1]
 
-alleylights= read.csv(textConnection(skip_first),header=T, fill = TRUE )
+readin <- function(wd,file,skip){
+  setwd(wd)
+  dataframe = readLines(file)
+  if (skip == "TRUE")
+  {
+    dataframe = dataframe[-1]
+  }
+  
+  data = read.csv(textConnection(dataframe),header=T, fill = TRUE )
+  
+  return(data)
+}
+
+alleylights <- readin('/Users/VirginiaSaulnier/Desktop/412_hw1','311_Service_Requests_-_Alley_Lights_Out.csv',TRUE)
+crime <- readin('/Users/VirginiaSaulnier/Desktop/412_hw1','Crimes2016.csv',FALSE)
+
 colnames(alleylights) <- c("Request","Status","Completion", "Number","Type", "Address", "Zip", "1","2","Ward","District","Community", "Latitude","Longitude","Location")
-names(alleylights)
 
-#alleylights$Request <- format(alleylights$Request, format="%m/%d/%y")
-#alleylights$Completion <- format(alleylights$Completion, format="%m/%d/%y")
-
-
-# based on variable values
-#newalley <- alleylights[ which(alleylights$Date >'01/01/2016' & alleylights$Status == 'completed') ]
-#alley16 <- select(alleylights,Request == '2016')
 alley16 <- alleylights[grep("2016", alleylights$Request), ]
 alley16$date_diff <- as.Date(as.character(alley16$Completion),format="%m/%d/%y")-
   as.Date(as.character(alley16$Request), format="%m/%d/%y")
-#district_diff <-  c(alley16$date_diff, alley16$district)
+
+
 service_ave = alley16 %>%
   group_by(District) %>%
-  summarise(avg = mean(date_diff, na.rm=TRUE)) %>%
-  arrange(avg)
+  summarise(repairtime = round(mean(date_diff, na.rm=TRUE),digits = 1)) %>%
+  arrange(District) 
 group_by(alley16, District)
+
+crime_counts = crime %>%
+  group_by(District) %>%
+  summarise(crimetotal=length(Primary.Type)) %>%
+arrange(District)
+
+results = full_join(crime_counts, service_ave, by = "District")%>%
+  arrange(crimetotal)
+
+#colMax <- function(data) sapply(data, max, na.rm = TRUE)
+dat$MAX <- apply(results[,-1],1,max,na.rm=TRUE) 
+
+cat("The district with the slowest repair time is the number of crimes this year was")
+cat("The district with the quickest repair time is the number of crimes this year was",dat$MAX)
